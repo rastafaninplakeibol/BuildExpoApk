16f2c2ad4ca6c0d4a3740e300eb6e2a5ebf52701
rastafaninplakeibol <rastafaninplakeibol@gmail.com>
2021-04-19 00:37:53 +0200

    updates, fixes and now it's bettet

--- README.md
+++ README.md
@@ -36,6 +36,7 @@ During the process, expo will take care of creating a unique keystore for your A
 You'll need to provide these informations in the json file, which will be parsed at the beginning of the process:
 ````
 {
+  "PUBLIC_URL": "",
 	"EXPO_ANDROID_KEYSTORE_PASSWORD": "",
 	"EXPO_ANDROID_KEY_PASSWORD": "",
 	"KEYSTORE_ALIAS": "",
@@ -45,9 +46,20 @@ You'll need to provide these informations in the json file, which will be parsed
 }
 ````
 
-To build the APK, a server will be launched using python3 http.server module, listening on port 8000 (you can modify the address and the port of the server in the buildApk.js file if you need it, it may be useful if you want to use a custom deployment server which can be somewhere else other than localhost).
+To build the APK, a server will be launched using python3 http.server module, listening on port 8000 (you can modify the address and the port of the server in the buildApk.js file if you need it, it may be useful if you want to use a custom deployment server which can be somewhere else other than localhost). This is the default behaviour if you don't populate the PUBLIC_URL field, otherwise the code will trust whatever you used as a public url to be well configured and waiting for connections. Remember that the PUBLIC_URL will be the default endpoint that your app will try to contact to check for new updates, so if it is a local address it will be hard to load updates directly in the APK without rebuilding everything.
 
 ## How to build
+
+**IMPORTANT** The code now works using the manifest link you get after running:
+````
+$ expo publish
+````
+which looks something like `https://exp.host/@yourusername/yourappname/index.exp?sdkVersion=x.y.z`. In this way you can update your standalone app just by running `expo publish` again and closing and opening again the app 2 times (for android, just one for ios). It is maybe the raccomended way as it avoid to export everything again and you dont need to launch any deployment server etc.
+
+If you don't want to rely on expo in any way you can go on reading the chapter.
+
+**N.B** Now there is no need as i've found the flag `--allow-non-https-public-url`, but if you run into some trouble because of the http link read the following chapter
+
 Before launching the build program, there is just a little tweak on the turtle source code that must be done, because turtle does not accept HTTP custom developement endpoints, and it tells you that only HTTPS is supported, which is a little lie.
 
 In order to avoid to get a valid, signed HTTPS certificate, you just need to go to `/usr/local/lib/node_modules/turtle-cli/build/bin/utils/builder.js` and look for:
--- buildApk.js
+++ buildApk.js
@@ -1,49 +1,42 @@
 const { spawn } = require('child_process');
 const fs = require('fs')
 
-const data = JSON.parse(fs.readFileSync('./expo_data.json'))
-console.log(data)
-
-process.env.EXPO_ANDROID_KEYSTORE_PASSWORD = data.EXPO_ANDROID_KEYSTORE_PASSWORD
-process.env.EXPO_ANDROID_KEY_PASSWORD = data.EXPO_ANDROID_KEY_PASSWORD
-process.env.EXPO_PASSWORD = data.EXPO_PASSWORD
-process.env.EXPO_USERNAME = data.EXPO_USERNAME
-
-
-
-if (fs.existsSync('./dist')) {
-	fs.rmdirSync('./dist', { recursive: true })
+function exportAndBuild() {
+	const exporter = spawn('expo', ['export', '--public-url', publicURL, '--dev']);
+	exporter.stdout.on('data', (chunk) => {
+		console.log('##EXPORT## ' + new String(chunk))
+	});
+	exporter.stderr.on('data', (data) => {
+		console.error('##EXPORT## ' + new String(data))
+	})
+	exporter.on('close', (code) => {
+		buildApk(true, code)
+	});
 }
 
-const exporter = spawn('expo', ['export', '--public-url', 'http://127.0.0.1:8000', '--dev']);
+function buildApk(exported, code) {
+	if (exported) console.log(`exporter process exited with code ${code}`);
+	if (!exported || exported && code == 0) {
+		if (publicURL.includes('localhost')) {
+			process.chdir('./dist')
 
-exporter.stdout.on('data', (chunk) => {
-	console.log('##EXPORT## ' + new String(chunk))
-});
-
-exporter.stderr.on('data', (data) => {
-	console.error('##EXPORT## ' + new String(data))
-})
-
-exporter.on('close', (code) => {
-	console.log(`exporter process exited with code ${code}`);
-	if (code == 0) {
-		process.chdir('./dist')
-		const pythonServer = spawn('python3', ['-m', 'http.server', '8000']);
-		pythonServer.stdout.on('data', (chunk) => {
-			console.log('##PSERVER## ' + new String(chunk))
-		});
-		pythonServer.stderr.on('data', (data) => {
-			console.error('##PSERVER## ' + new String(data))
-		})
-		process.chdir('..')
+			pythonServer = spawn('python3', ['-m', 'http.server', '8000']);
+			pythonServer.stdout.on('data', (chunk) => {
+				console.log('##PSERVER## ' + new String(chunk))
+			});
+			pythonServer.stderr.on('data', (data) => {
+				console.error('##PSERVER## ' + new String(data))
+			})
+			process.chdir('..')
+		}
 		const turtleBuilder = spawn('turtle', [
 			'build:android',
 			'--type', 'apk',
 			'--keystore-alias', data.KEYSTORE_ALIAS,
 			'--keystore-path', data.KEYSTORE_PATH,
-			'--public-url', 'http://127.0.0.1:8000/android-index.json',
+			'--public-url', `${publicURL}/android-index.json`,
 			'--build-dir', './build',
+			allowHTTP ? '--allow-non-https-public-url' : ''
 		]);
 
 		turtleBuilder.stdout.on('data', (data) => {
@@ -61,8 +54,34 @@ exporter.on('close', (code) => {
 				pythonServer.kill()
 				process.exit(-1)
 			}
-			pythonServer.kill()
+			if (pythonServer) pythonServer.kill()
 			process.exit(0)
 		})
 	}
-});
+}
+
+const data = JSON.parse(fs.readFileSync('./expo_data.json'))
+console.log(data)
+
+process.env.EXPO_ANDROID_KEYSTORE_PASSWORD = data.EXPO_ANDROID_KEYSTORE_PASSWORD
+process.env.EXPO_ANDROID_KEY_PASSWORD = data.EXPO_ANDROID_KEY_PASSWORD
+process.env.EXPO_PASSWORD = data.EXPO_PASSWORD
+process.env.EXPO_USERNAME = data.EXPO_USERNAME
+
+const publicURL = data.PUBLIC_URL || 'http://localhost:8000'
+const allowHTTP = publicURL ? (publicURL.includes('http://') ? true : false) : true
+const isExpoManifestUrl = publicURL.includes('https://exp.host')
+var pythonServer = null
+
+if (fs.existsSync('./dist')) {
+	fs.rmdirSync('./dist', { recursive: true })
+}
+if (fs.existsSync('./ios')) {
+	fs.rmdirSync('./ios', { recursive: true })
+}
+if (fs.existsSync('./android')) {
+	fs.rmdirSync('./android', { recursive: true })
+}
+
+if (isExpoManifestUrl) buildApk(false, 0)
+else exportAndBuild()
